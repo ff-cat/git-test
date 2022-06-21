5faee1549ecb97642beb59ddf4e8a1898eb38fdb
ff-cat <fatflycat@gmail.com>
2022-06-21 15:59:27 +0300

    add setTimeout log(GoHome)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './App.css'
 
 function App() {
+    setTimeout(()=> {
+        console.log('GoHome')
+    },5000)
+
+
     return (
         <h1>Hello world!</h1>
     )
